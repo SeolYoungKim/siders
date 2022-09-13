881794bebbdb65290545f3bca54a50ef5246bc6d
SeolYoungKim <nasur4da@gmail.com>
2022-09-13 20:36:55 +0900

    [START] 프로젝트 초기 구성

---
+++ src/test/java/com/example/siderswebapp/SidersWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siderswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidersWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
