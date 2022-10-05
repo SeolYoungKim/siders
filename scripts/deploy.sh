d68da69926ddb558b04fda6a2a4582de6328eb54
SeolYoungKim <nasur4da@gmail.com>
2022-10-05 12:06:21 +0900

    [Test] 배포 테스트 kill -15 한번 더 수행

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl siders-webapp | grep java | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션의 pid: $CURRENT_PID"
+echo "> 현재 구동 중인 애플리케이션의 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -27,8 +27,8 @@ fi
 if [ -z "$CURRENT_PID" ]; then
   echo "> 프로세스가 정상적으로 종료되었습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 10
 fi
 
