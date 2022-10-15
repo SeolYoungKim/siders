054ed86cd1de5a9cf08f4bb3b3c78407e2ef4486
SeolYoungKim <nasur4da@gmail.com>
2022-10-15 18:07:33 +0900

    [Test] cors test

--- src/main/java/com/example/siderswebapp/config/WebConfig.java
+++ src/main/java/com/example/siderswebapp/config/WebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static com.example.siderswebapp.auth.UriList.FRONT_END;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -12,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         long MAX_AGE_SECS = 3600;
 
         registry.addMapping("/**")
-                .allowedOrigins("https://admirable-starburst-00d6a9.netlify.app/")
+                .allowedOrigins("https://admirable-starburst-00d6a9.netlify.app/", FRONT_END.getUri())
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
