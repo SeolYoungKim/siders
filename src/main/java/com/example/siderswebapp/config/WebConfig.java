8f6ed91c9f55ad4d09a64a4049d776457deb4e3f
SeolYoungKim <nasur4da@gmail.com>
2022-10-17 14:54:28 +0900

    [Test] Cors test

--- src/main/java/com/example/siderswebapp/config/WebConfig.java
+++ src/main/java/com/example/siderswebapp/config/WebConfig.java
@@ -1,23 +1,19 @@
 package com.example.siderswebapp.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static com.example.siderswebapp.auth.UriList.FRONT_END;
-
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        long MAX_AGE_SECS = 3600;
-
-        registry.addMapping("/**")
-                .allowedOrigins("https://admirable-starburst-00d6a9.netlify.app/", FRONT_END.getUri())
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        long MAX_AGE_SECS = 3600;
+//
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://admirable-starburst-00d6a9.netlify.app/", FRONT_END.getUri())
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(MAX_AGE_SECS);
+//    }
 }
